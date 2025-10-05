4119c3cf7155c18e751653770e28864864b2c160
AhmedDev <ahmedkhanahmedkhan7006@gmail.com>
2025-10-05 14:11:55 +0500

    Add files via upload

---
+++ Calculator App.py
@@ -0,0 +1,7 @@
+num1 = int(input("First Number: "))
+num2 = int(input("Second Number: "))
+
+print("Addition: ", num1 + num2)
+print("Substraction: ", num1 - num2)
+print("Multiplication: ", num1 * num2)
+print("Division: ", num1 / num2)
